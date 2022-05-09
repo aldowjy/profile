8076cf570f1d9e723337e9f4e9031e56bb79bc55
Aldo Wijaya <aldowjy12@gmail.com>
2022-05-09 20:02:21 +0700

    fix layout design responsive

--- src/App.js
+++ src/App.js
@@ -12,53 +12,77 @@ import {
   HStack,
   Container,
   Image,
+  Tab,
+  Tabs,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Hide,
 } from "@chakra-ui/react";
 import { PhoneIcon } from "@chakra-ui/icons";
 
 function App() {
   return (
-    <div className="App" bg="#1f2235">
-      <Container maxW="container.xl" bg="#1f2235">
+    <div className="App" bg="default.500">
+      <Container maxW="container.xl" bg="default.500">
         <Flex>
-          <Text p={4} color="white" style={{ fontWeight: "bold" }}>
+          <Text p={4} color="primary.500" style={{ fontWeight: "bold" }}>
             dovin
           </Text>
           <Spacer />
-          <HStack p={4} spacing="24px">
-            <Text color="white" style={{ fontWeight: "bold" }}>
+          <HStack p={4} spacing={10}>
+            <Button variant="ghost" size={["xs", "sm"]}>
               Home
-            </Text>
-            <Text color="white" style={{ fontWeight: "bold" }}>
-              Services
-            </Text>
-            <Text color="white" style={{ fontWeight: "bold" }}>
-              About
-            </Text>
-            <Text color="white" style={{ fontWeight: "bold" }}>
-              Skills
-            </Text>
-            <Text color="white" style={{ fontWeight: "bold" }}>
-              Portfolio
-            </Text>
-            <Text color="white" style={{ fontWeight: "bold" }}>
-              Contact
-            </Text>
+            </Button>
+            <Hide below="md">
+              <Button variant="ghost" size={["xs", "sm"]}>
+                Services
+              </Button>
+              <Button variant="ghost" size={["xs", "sm"]}>
+                About
+              </Button>
+              <Button variant="ghost" size={["xs", "sm"]}>
+                Skills
+              </Button>
+              <Button variant="ghost" size={["xs", "sm"]}>
+                Portfolio
+              </Button>
+              <Button variant="ghost" size={["xs", "sm"]}>
+                Contact
+              </Button>
+            </Hide>
           </HStack>
         </Flex>
       </Container>
 
-      <Box p={16} bg="#23263a">
-        <Box m={16} p={16}>
-          <Heading as="h3" size="lg" mb={2} color="#ff4a56">
+      <Box p={[4, 6, 8, 16]} bg="#23263a">
+        <Box m={[4, 6, 8, 16]} p={[4, 6, 8, 16]}>
+          <Heading
+            as="h2"
+            size="xl"
+            mb={2}
+            color="primary.500"
+            fontSize={["sm", "md", "lg", "xl"]}
+          >
             Hello
           </Heading>
-          <Heading as="h2" size="3xl" mb={2}>
+          <Heading
+            as="h1"
+            size="4xl"
+            mb={2}
+            fontSize={["xl", "2xl", "3xl", "4xl"]}
+          >
             I am Aldo Wijaya
           </Heading>
-          <Heading as="h4" size="md" mb={8}>
-            i'm Aldo, professional web developer, mobile developer, and a gamer.
+          <Heading
+            as="h3"
+            size="lg"
+            mb={[4, 6, 8, 16]}
+            fontSize={["xs", "sm", "md", "lg"]}
+          >
+            professional web and mobile developer, freelance and a gamer.
           </Heading>
-
+          <Button variant="outline">My Portfolio</Button>
           {/* <PhoneIcon />
           <PhoneIcon />
           <PhoneIcon />
@@ -67,86 +91,94 @@ function App() {
         </Box>
       </Box>
 
-      <Box p={16}>
+      <Box p={[4, 6, 8, 16]}>
         <Center mb={2}>
-          <Text fontSize="lg" color="#ff4a56">
+          <Text fontSize={["sm", "md", "lg", "xl"]} color="primary.500">
             SERVICES
           </Text>
         </Center>
         <Center mb={2}>
-          <Text fontSize="xl" style={{ fontWeight: "bold" }}>
+          <Text
+            fontSize={["md", "lg", "xl", "2xl"]}
+            style={{ fontWeight: "bold" }}
+          >
             My Services
           </Text>
         </Center>
-        <Center mb={16}>
-          <Text fontSize="md">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        <Center mb={[4, 6, 8, 16]}>
+          <Text fontSize={["xs", "sm", "md", "lg"]} align={["center"]}>
+            below are some services that you can use.
           </Text>
         </Center>
         <Center>
-          <SimpleGrid columns={3} spacing={10} mb={8}>
-            <Box bg="#ff4a56" height="200px" width="350px" p={4}>
-              <Text fontSize="lg" style={{ fontWeight: "bold" }} mb={8}>
+          <SimpleGrid columns={[2, 3, 4]} spacing={10} mb={8}>
+            <Box bg="primary.500" h={[250]} w={[150, 250, 350]} p={4}>
+              <Text
+                fontSize={["sm", "md", "lg", "xl"]}
+                style={{ fontWeight: "bold" }}
+                mb={8}
+              >
                 Web Development
               </Text>
-              <Text fontSize="md">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              <Text fontSize={["xs", "sm", "md", "lg"]}>
+                web development for personal branding, wedding invitations,
+                operating systems, managing dashboards, etc.
               </Text>
             </Box>
-            <Box bg="#ff4a56" height="200px" width="350px" p={4}>
-              <Text fontSize="lg" style={{ fontWeight: "bold" }} mb={8}>
+            <Box bg="primary.500" h={[250]} w={[150, 250, 350]} p={4}>
+              <Text
+                fontSize={["sm", "md", "lg", "xl"]}
+                style={{ fontWeight: "bold" }}
+                mb={8}
+              >
                 Mobile Development
               </Text>
-              <Text fontSize="md">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              <Text fontSize={["xs", "sm", "md", "lg"]}>
+                mobile development for managing cashier work, recording income
+                and expenses, etc.
               </Text>
             </Box>
-            <Box bg="#ff4a56" height="200px" width="350px" p={4}>
-              <Text fontSize="lg" style={{ fontWeight: "bold" }} mb={8}>
+            <Box bg="primary.500" h={[250]} w={[150, 250, 350]} p={4}>
+              <Text
+                fontSize={["sm", "md", "lg", "xl"]}
+                style={{ fontWeight: "bold" }}
+                mb={8}
+              >
                 Video Editing
               </Text>
-              <Text fontSize="md">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              <Text fontSize={["xs", "sm", "md", "lg"]}>
+                I am able to do basic video editing such as making After Event
+                Videos, Game Highlights, etc.
               </Text>
             </Box>
-          </SimpleGrid>
-        </Center>
-        <Center>
-          <SimpleGrid columns={2} spacing={10}>
-            <Box bg="#ff4a56" height="200px" width="350px" p={4}>
-              <Text fontSize="lg" style={{ fontWeight: "bold" }} mb={8}>
+            <Box bg="primary.500" h={[250]} w={[150, 250, 350]} p={4}>
+              <Text
+                fontSize={["sm", "md", "lg", "xl"]}
+                style={{ fontWeight: "bold" }}
+                mb={8}
+              >
                 Game Streamer
               </Text>
-              <Text fontSize="md">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </Text>
-            </Box>
-            <Box bg="#ff4a56" height="200px" width="350px" p={4}>
-              <Text fontSize="lg" style={{ fontWeight: "bold" }} mb={8}>
-                Gamer as Hobby
-              </Text>
-              <Text fontSize="md">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              <Text fontSize={["xs", "sm", "md", "lg"]}>
+                besides being a developer, on the other hand I have a hobby of
+                playing online games
               </Text>
             </Box>
           </SimpleGrid>
         </Center>
       </Box>
 
-      <Box p={16} bg="#23263a">
-        <Center mb={16}>
-          <Text fontSize="xl" style={{ fontWeight: "bold" }}>
+      <Box p={[4, 6, 8, 16]} bg="#23263a">
+        <Center mb={[4, 6, 8, 16]}>
+          <Text
+            fontSize={["md", "lg", "xl", "2xl"]}
+            style={{ fontWeight: "bold" }}
+          >
             About Me
           </Text>
         </Center>
         <Center>
-          <SimpleGrid columns={2} spacing={10}>
+          <SimpleGrid columns={[1, 2]} spacing={10}>
             <Box height="300px" m={4} p={4}>
               <Flex dir="row">
                 <Spacer />
@@ -155,14 +187,14 @@ function App() {
             </Box>
             <Box height="300px" m={4} p={4}>
               <Text
-                fontSize="xl"
-                color="#ff4a56"
+                fontSize={["md", "lg", "xl", "2xl"]}
+                color="primary.500"
                 style={{ fontWeight: "bold" }}
                 mb={8}
               >
                 Hi There
               </Text>
-              <Text fontSize="md">
+              <Text fontSize={["xs", "sm", "md", "lg"]}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
                 enim ad minim veniam, quis nostrud exercitation ullamco laboris
@@ -176,99 +208,136 @@ function App() {
         </Center>
       </Box>
 
-      <Box p={16}>
+      <Box p={[4, 6, 8, 16]}>
         <Center mb={2}>
-          <Text fontSize="xl" style={{ fontWeight: "bold" }}>
+          <Text
+            fontSize={["md", "lg", "xl", "2xl"]}
+            style={{ fontWeight: "bold" }}
+          >
             My Skills
           </Text>
         </Center>
-        <Center mb={16}>
-          <Text fontSize="md">
+        <Center mb={[4, 6, 8, 16]}>
+          <Text fontSize={["xs", "sm", "md", "lg"]} align={["center"]}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </Text>
         </Center>
         <Center>
-          <SimpleGrid columns={4} spacing={10} mb={8}>
-            <Box bg="#ff4a56" height="200px" width="300px" p={4}>
+          <SimpleGrid columns={[2, 3, 4]} spacing={10} mb={8}>
+            <Box bg="primary.500" h={[100, 150, 200]} w={[150, 200, 300]} p={4}>
               <Center>
-                <Text fontSize="4xl">React JS</Text>
+                <Text fontSize={["xl", "2xl", "3xl", "4xl"]}>React JS</Text>
               </Center>
             </Box>
-            <Box bg="gray" height="200px" width="300px" p={4}>
+            <Box bg="gray" h={[100, 150, 200]} w={[150, 200, 300]} p={4}>
               <Center>
-                <Text fontSize="4xl">Vue</Text>
+                <Text fontSize={["xl", "2xl", "3xl", "4xl"]}>Vue</Text>
               </Center>
             </Box>
-            <Box bg="#ff4a56" height="200px" width="300px" p={4}>
+            <Box bg="primary.500" h={[100, 150, 200]} w={[150, 200, 300]} p={4}>
               <Center>
-                <Text fontSize="4xl">Android</Text>
+                <Text fontSize={["xl", "2xl", "3xl", "4xl"]}>Android</Text>
               </Center>
             </Box>
-            <Box bg="gray" height="200px" width="300px" p={4}>
+            <Box bg="gray" h={[100, 150, 200]} w={[150, 200, 300]} p={4}>
               <Center>
-                <Text fontSize="4xl">Premiere</Text>
+                <Text fontSize={["xl", "2xl", "3xl", "4xl"]}>Premiere</Text>
               </Center>
             </Box>
           </SimpleGrid>
         </Center>
         <Center>
-          <SimpleGrid columns={3} spacing={10}>
-            <Box bg="gray" height="200px" width="300px" p={4}>
+          <SimpleGrid columns={[2, 4]} spacing={10}>
+            <Box bg="gray" h={[100, 150, 200]} w={[150, 200, 300]} p={4}>
               <Center>
-                <Text fontSize="4xl">HTML</Text>
+                <Text fontSize={["xl", "2xl", "3xl", "4xl"]}>HTML</Text>
               </Center>
             </Box>
-            <Box bg="#ff4a56" height="200px" width="300px" p={4}>
+            <Box bg="primary.500" h={[100, 150, 200]} w={[150, 200, 300]} p={4}>
               <Center>
-                <Text fontSize="4xl">CSS</Text>
+                <Text fontSize={["xl", "2xl", "3xl", "4xl"]}>CSS</Text>
               </Center>
             </Box>
-            <Box bg="gray" height="200px" width="300px" p={4}>
+            <Box bg="gray" h={[100, 150, 200]} w={[150, 200, 300]} p={4}>
               <Center>
-                <Text fontSize="4xl">Javascript</Text>
+                <Text fontSize={["xl", "2xl", "3xl", "4xl"]}>Javascript</Text>
               </Center>
             </Box>
           </SimpleGrid>
         </Center>
       </Box>
 
-      <Box p={16} bg="#23263a">
+      <Box p={[4, 6, 8, 16]} bg="#23263a">
         <Center mb={8}>
-          <Text fontSize="xl" style={{ fontWeight: "bold" }}>
+          <Text
+            fontSize={["md", "lg", "xl", "2xl"]}
+            style={{ fontWeight: "bold" }}
+          >
             Portfolio
           </Text>
         </Center>
         <Center>
-          <Stack spacing={4} direction="row" align="center" mb={16}>
-            <Button color="#ff4a56" size="md">
-              All Works
-            </Button>
-            <Button color="#ff4a56" size="md">
-              Website
-            </Button>
-            <Button color="#ff4a56" size="md">
-              Mobile
-            </Button>
-            <Button color="#ff4a56" size="md">
-              Design
-            </Button>
-            <Button color="#ff4a56" size="md">
-              Video
-            </Button>
-          </Stack>
+          <Tabs variant="soft-rounded" colorScheme="primary" align="center">
+            <TabList>
+              <Tab mx={2}>All Works</Tab>
+              <Hide below="md">
+                <Tab mx={2}>Website</Tab>
+                <Tab mx={2}>Mobile</Tab>
+                <Tab mx={2}>Design</Tab>
+                <Tab mx={2}>Video</Tab>
+              </Hide>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <HStack p={2}>
+                  <Image src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
+                </HStack>
+                <HStack p={2}>
+                  <Image src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
+                </HStack>
+              </TabPanel>
+              <TabPanel>
+                <HStack p={2}>
+                  <Image src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
+                </HStack>
+              </TabPanel>
+              <TabPanel>
+                <HStack p={2}>
+                  <Image src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
+                </HStack>
+              </TabPanel>
+              <TabPanel>
+                <HStack p={2}>
+                  <Image src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
+                </HStack>
+              </TabPanel>
+              <TabPanel>
+                <HStack p={2}>
+                  <Image src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
+                </HStack>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
         </Center>
       </Box>
 
-      <Box p={16}>
-        <Box p={16}>
+      <Box p={[4, 6, 8, 16]}>
+        <Box p={[4, 6, 8, 16]}>
           <Center mb={2}>
-            <Text fontSize="xl" style={{ fontWeight: "bold" }}>
+            <Text
+              fontSize={["md", "lg", "xl", "2xl"]}
+              style={{ fontWeight: "bold" }}
+            >
               Contact Me
             </Text>
           </Center>
           <Center mb={8}>
-            <Text fontSize="md" color="#ff4a56">
+            <Text
+              fontSize={["xs", "sm", "md", "lg"]}
+              color="primary.500"
+              align={["center"]}
+            >
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </Text>
--- src/index.js
+++ src/index.js
@@ -8,14 +8,75 @@ import { extendTheme, ChakraProvider } from "@chakra-ui/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const colors = {
-  brand: {
-    900: "#1a365d",
-    800: "#153e75",
-    700: "#2a69ac",
+  default: {
+    500: "#1f2235",
+  },
+  primary: {
+    100: "#ff4a56",
+    500: "#ff4a56",
   },
 };
 
-const theme = extendTheme({ colors });
+const Button = {
+  // The styles all Cards have in common
+  baseStyle: {
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    borderRadius: "base", // <-- border radius is same for all variants and sizes
+  },
+  // Two sizes: sm and md
+  sizes: {
+    sm: {
+      fontSize: "sm",
+      px: 4, // <-- px is short for paddingLeft and paddingRight
+      py: 3, // <-- py is short for paddingTop and paddingBottom
+    },
+    md: {
+      fontSize: "md",
+      px: 6, // <-- these values are tokens from the design system
+      py: 4, // <-- these values are tokens from the design system
+    },
+  },
+  // Two variants: outline and solid
+  variants: {
+    outline: {
+      border: "2px solid",
+      borderColor: "#ff4a56",
+      color: "#ff4a56",
+      _hover: {
+        bg: "#ff4a56",
+        color: "white",
+      },
+    },
+    solid: {
+      bg: "#ff4a56",
+      color: "white",
+      _hover: {
+        bg: "#e6000f",
+      },
+    },
+    ghost: {
+      bg: "transparent",
+      color: "#ff4a56",
+      _hover: {
+        bg: "#ff4a56",
+        color: "white",
+      },
+    },
+  },
+  // The default size and variant values
+  defaultProps: {
+    size: "md",
+    variant: "outline",
+  },
+};
+
+const theme = extendTheme({
+  colors,
+  components: {
+    Button,
+  },
+});
 
 root.render(
   <React.StrictMode>
